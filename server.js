599b2fb20c57302730fb0f31a8c8fc0bb9ff1279
chucruwt <daroltpietro336@gmail.com>
2026-08-31 11:38:27 -0300

    fix: adiciona host 0.0.0.0 para o render

--- server.js
+++ server.js
@@ -64,6 +64,8 @@ server.delete('/videos/:id', async (request, reply) => {
 })
 
 server.listen({
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: process.env.PORT ? Number(process.env.PORT) : 3333,
+}).then(() => {
+    console.log('HTTP Server Running!')
 })
